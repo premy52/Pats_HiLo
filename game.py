610cdf6317dee71e50f7fbc6c91b82f485442795
Pat Remy <1796247+premy52@users.noreply.github.com>
2022-03-07 14:50:47 -0800

    First commit on my little game

---
+++ game.py
@@ -0,0 +1,26 @@
+# my little game
+import random
+
+tries = 0
+answer = random.randint(1, 100)
+chances = 5
+
+while tries < chances:
+    print('------------------\nanswer is ', answer)
+    guess = input('Enter an integer between 1 and 100: ')
+    guess_int = int(guess)
+    tries += 1
+    if 1 <= guess_int <= 100:
+        print(f'Trying {guess_int}')
+        if guess_int == answer:
+            print("Correct answer of", answer, "in ", tries, " tries")
+            break
+        elif guess_int > answer:
+            print("Too high:", chances - tries, " tries remaining")
+        elif guess_int < answer:
+            print("Too low: ", chances - tries, " tries remaining")
+        else:
+            print("...we shouldn't be here")
+    else:
+        print("follow the instructions; you're done here")
+        break
